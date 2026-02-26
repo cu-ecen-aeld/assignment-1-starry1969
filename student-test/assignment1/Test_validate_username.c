ed383ca84ef9e2491c053cba03d699ed7a296390
starry69 <user@Latitude-5580.mynetworksettings.com>
2026-02-26 05:12:17 -0800

    Complete assignment 1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,14 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *hardcoded = my_username();
+    char *fromconf = malloc_username_from_conf_file();
+
+    TEST_ASSERT_NOT_NULL_MESSAGE(hardcoded, "my_username() returned NULL");
+    TEST_ASSERT_NOT_NULL_MESSAGE(fromconf, "malloc_username_from_conf_file() returned NULL");
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded, fromconf,
+        "conf/username.txt does not match my_username()");
+
+    free(fromconf);
 }
